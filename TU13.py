b48a7734eb1ebcf18122e60859441e03109e679d
AravBende <92068151+AravBende@users.noreply.github.com>
2022-11-14 19:25:45 +0800

    Create TU13.py

---
+++ TU13.py
@@ -0,0 +1,19 @@
+# excersize 1
+numbers = [12,31,43,53]
+
+print(sum(numbers))
+
+# excersize 2
+print(sum(numbers))
+
+print(max(numbers))
+
+print(min(numbers))
+
+print(len(numbers))
+
+print(sum(numbers)/len(numbers))
+
+print(sum(numbers))
+
+
