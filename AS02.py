93fd8c798fea056ba5c35b1f443f169e01c88c44
Arav Bende <92068151+AravBende@users.noreply.github.com>
2023-02-07 12:22:23 +0800

    Create AS02.py

---
+++ AS02.py
@@ -0,0 +1,11 @@
+Number = 29
+items = [11, 25, 10, 29, 15, 13, 18]
+step = 0
+
+for item in items:
+  if item == Number:
+    print("Number found")
+    print(step)
+  else:
+    step = step + 1
+  
